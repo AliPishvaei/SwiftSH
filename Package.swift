f248c5498ebb12dacc752f2b71a2451ab149e56a
Ali Pishvaei <AliPishvaee@Gmail.com>
2025-06-12 18:06:18 +0200

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,43 +1,36 @@
- // swift-tools-version:5.3
- import PackageDescription
+// swift-tools-version:5.3
+import PackageDescription
 
- let package = Package(
-     name: "SwiftSH",
-     platforms: [
-         .iOS(.v8)
-     ],
-     products: [
-         .library(
-             name: "SwiftSH",
-             targets: ["Libssh2"]
-         )
-     ],
-     targets: [
-         .target(
-             name: "Core",
-             path: "SwiftSH",
-             exclude: ["Libssh2.h", "Libssh2.m"],
-             sources: [
-                 // All Swift files except Libssh2*.{h,m,swift}
-             ],
-             publicHeadersPath: ""
-         ),
-         .target(
-             name: "Libssh2",
-             dependencies: ["Core"],
-             path: "SwiftSH",
-             sources: ["Libssh2.h", "Libssh2.m", "Libssh2.swift"],
-             publicHeadersPath: "libssh2",
-             cSettings: [
-                 .headerSearchPath("libssh2")
-             ],
-             linkerSettings: [
-                 .linkedLibrary("z"),
-                 .unsafeFlags([
-                     "-L$(PODS_ROOT)/SwiftSH/libssh2",
-                     "-F$(PODS_ROOT)/SwiftSH/libssh2"
-                 ])
-             ]
-         )
-     ]
- )
+let package = Package(
+    name: "SwiftSH",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "SwiftSH",
+            targets: ["SwiftSH"]),
+    ],
+    dependencies: [],
+    targets: [
+        .systemLibrary(
+            name: "libssh2",
+            pkgConfig: "libssh2",
+            providers: [
+                .brew(["libssh2"]),
+                .apt(["libssh2-1-dev"]),
+            ]
+        ),
+        .target(
+            name: "SwiftSH",
+            dependencies: ["libssh2"],
+            path: "SwiftSH"
+        ),
+        .testTarget(
+            name: "SwiftSHTests",
+            dependencies: ["SwiftSH"],
+            path: "SwiftSHTests"
+        ),
+    ]
+)
